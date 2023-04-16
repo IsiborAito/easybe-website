41fa442d05aecef11462c4ef177c45905db880ee
Isibor Aito <calebaito@easybe.io>
2023-04-16 20:58:22 +0200

    created the whyUs page
    Have the basic page created with the App page showing the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import WhyUs from "./components/whyUs";
+import { ReactLocation, Route, Router } from '@tanstack/react-location'
 import './App.css';
+
+const location = new ReactLocation({});
+
+const routes: Route[] = [
+  {
+    path: '',
+    children: [
+      {path: '/why-us',
+      element: <WhyUs/>}
+    ]
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router location={location} routes={routes}/>
   );
 }
 
